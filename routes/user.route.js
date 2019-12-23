d54fe5815a4258dd218b093c804dff1fea4b4b3e
roopkumarr <rpkmrroopkumar8@gmail.com>
2019-12-23 12:35:18 +0530

    updated the controllers routes and tests file to support the CRUD operations

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const controller = require("../controllers/user.controller");
 
-
+router
+  .route("/")
+  .get(controller.getAllUsers)
+  .post(controller.createUser);
+router
+  .route("/:id")
+  .get(controller.getUser)
+  .put(controller.updateUser)
+  .delete(controller.deleteUser);
 
 module.exports = router;
